e350600af7a4deac41b60143a6ab46ede2605add
Fernando Cruz <65718224+cruz-f@users.noreply.github.com>
2022-09-26 17:12:22 +0100

    [ADD] adding select k best; f_classification; f_regression

---
+++ src/si/statistics/f_classification.py
@@ -0,0 +1,31 @@
+from typing import Tuple, Union
+
+import numpy as np
+from scipy import stats
+
+from si.data.dataset import Dataset
+
+
+def f_classification(dataset: Dataset) -> Union[Tuple[np.ndarray, np.ndarray],
+                                                Tuple[float, float]]:
+    """
+    Scoring function for classification problems. It computes one-way ANOVA F-value for the
+    provided dataset. The F-value scores allows analyzing if the mean between two or more groups (factors)
+    are significantly different. Samples are grouped by the labels of the dataset.
+
+    Parameters
+    ----------
+    dataset: Dataset
+        A labeled dataset
+
+    Returns
+    -------
+    F: np.array, shape (n_features,)
+        F scores
+    p: np.array, shape (n_features,)
+        p-values
+    """
+    classes = dataset.get_classes()
+    groups = [dataset.X[dataset.y == c] for c in classes]
+    F, p = stats.f_oneway(*groups)
+    return F, p
