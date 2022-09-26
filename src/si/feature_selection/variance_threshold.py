2f03bb391ee7d044a1bf330fcb6a94511e8ee3ab
Fernando Cruz <65718224+cruz-f@users.noreply.github.com>
2022-09-26 15:33:22 +0100

    [ADD] adding variance threshold

---
+++ src/si/feature_selection/variance_threshold.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+from si.data.dataset import Dataset
+
+
+class VarianceThreshold:
+    """
+    Variance Threshold feature selection.
+    Features with a training-set variance lower than this threshold will be removed from the dataset.
+
+    Parameters
+    ----------
+    threshold: float
+        The threshold value to use for feature selection. Features with a
+        training-set variance lower than this threshold will be removed.
+
+    Attributes
+    ----------
+    variance: array-like, shape (n_features,)
+        The variance of each feature.
+    """
+
+    def __init__(self, threshold: float = 0.0):
+        """
+        Variance Threshold feature selection.
+        Features with a training-set variance lower than this threshold will be removed from the dataset.
+
+        Parameters
+        ----------
+        threshold: float
+            The threshold value to use for feature selection. Features with a
+            training-set variance lower than this threshold will be removed.
+        """
+        if threshold < 0:
+            raise ValueError("Threshold must be non-negative")
+
+        # parameters
+        self.threshold = threshold
+
+        # attributes
+        self.variance = None
+
+    def fit(self, dataset: Dataset) -> 'VarianceThreshold':
+        """
+        Fit the VarianceThreshold model according to the given training data.
+        Parameters
+        ----------
+        dataset : Dataset
+            The dataset to fit.
+
+        Returns
+        -------
+        self : object
+        """
+        self.variance = np.var(dataset.X, axis=0)
+        return self
+
+    def transform(self, dataset: Dataset) -> Dataset:
+        """
+        It removes all features whose variance does not meet the threshold.
+        Parameters
+        ----------
+        dataset: Dataset
+
+        Returns
+        -------
+        dataset: Dataset
+        """
+        X = dataset.X
+
+        features_mask = self.variance > self.threshold
+        X = X[:, features_mask]
+        return Dataset(X, dataset.y, dataset.features[features_mask], dataset.label)
+
+    def fit_transform(self, dataset: Dataset) -> Dataset:
+        """
+        Fit to data, then transform it.
+        Parameters
+        ----------
+        dataset: Dataset
+
+        Returns
+        -------
+        dataset: Dataset
+        """
+        self.fit(dataset)
+        return self.transform(dataset)
