96e2e9275132784e6fa514dadfc1c0b682d8ec43
Fernando Cruz <65718224+cruz-f@users.noreply.github.com>
2022-10-21 09:50:49 +0100

    [FIX] fixing the ridge regression

--- src/si/linear_model/ridge_regression.py
+++ src/si/linear_model/ridge_regression.py
@@ -21,8 +21,11 @@ class RidgeRegression:
     Attributes
     ----------
     theta: np.array
-        The model parameters, namely the coefficiens of the linear model.
+        The model parameters, namely the coefficients of the linear model.
         For example, x0 * theta[0] + x1 * theta[1] + ...
+    theta_zero: float
+        The model parameter, namely the intercept of the linear model.
+        For example, theta_zero * 1
     """
     def __init__(self, l2_penalty: float = 1, alpha: float = 0.001, max_iter: int = 1000):
         """
@@ -43,6 +46,7 @@ class RidgeRegression:
 
         # attributes
         self.theta = None
+        self.theta_zero = None
 
     def fit(self, dataset: Dataset) -> 'RidgeRegression':
         """
@@ -62,21 +66,22 @@ class RidgeRegression:
 
         # initialize the model parameters
         self.theta = np.zeros(n)
+        self.theta_zero = 0
 
         # gradient descent
         for i in range(self.max_iter):
             # predicted y
-            y_pred = np.dot(dataset.X, self.theta)
+            y_pred = np.dot(dataset.X, self.theta) + self.theta_zero
 
-            # compute the gradient
-            gradient = self.alpha * (1 / m) * np.dot(y_pred - dataset.y, dataset.X)
+            # computing and updating the gradient with the learning rate
+            gradient = (self.alpha * (1 / m)) * np.dot(y_pred - dataset.y, dataset.X)
 
-            # compute the penalty
+            # computing the penalty
             penalization_term = self.alpha * (self.l2_penalty / m) * self.theta
-            penalization_term[0] = 0
 
-            # update the model parameters
-            self.theta = self.theta - penalization_term - gradient
+            # updating the model parameters
+            self.theta = self.theta - gradient - penalization_term
+            self.theta_zero = self.theta_zero - (self.alpha * (1 / m)) * np.sum(y_pred - dataset.y)
 
         return self
 
@@ -94,7 +99,7 @@ class RidgeRegression:
         predictions: np.array
             The predictions of the dataset
         """
-        return np.dot(dataset.X, self.theta)
+        return np.dot(dataset.X, self.theta) + self.theta_zero
 
     def score(self, dataset: Dataset) -> float:
         """
@@ -134,16 +139,27 @@ class RidgeRegression:
 if __name__ == '__main__':
     # import dataset
     from si.data.dataset import Dataset
-    from si.model_selection.split import train_test_split
 
-    # load and split the dataset
-    dataset_ = Dataset.from_random(600, 100, 100)
-    dataset_train, dataset_test = train_test_split(dataset_, test_size=0.2)
+    # make a linear dataset
+    X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    y = np.dot(X, np.array([1, 2])) + 3
+    dataset_ = Dataset(X=X, y=y)
 
     # fit the model
-    model = RidgeRegression(l2_penalty=1, alpha=0.001, max_iter=1000)
-    model.fit(dataset_train)
+    model = RidgeRegression()
+    model.fit(dataset_)
+
+    # get coefs
+    print(f"Parameters: {model.theta}")
 
     # compute the score
-    score = model.score(dataset_test)
+    score = model.score(dataset_)
     print(f"Score: {score}")
+
+    # compute the cost
+    cost = model.cost(dataset_)
+    print(f"Cost: {cost}")
+
+    # predict
+    y_pred_ = model.predict(Dataset(X=np.array([[3, 5]])))
+    print(f"Predictions: {y_pred_}")
