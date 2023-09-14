6a16e3582970d400d48016455dc480a019d53873
João Correia <jfscorreia95@gmail.com>
2023-09-14 17:30:12 +0100

    [ADD] co-autorship

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,12 +1,12 @@
 __author__ = "João Correia"
-__credits__ = ["Fernando Cruz", "Vítor Pereira"]
+__credits__ = ["João Correia, Fernando Cruz", "Vítor Pereira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "João Correia"
 __email__ = "jfscorreia95@gmail.com"
 
 # -----------------------------------------------------------------------------------
-# This package is heavily inspired and adapted from https://github.com/vmspereira/si
-# and
-# https://github.com/cruz-f/si
+# This package is heavily inspired and adapted from
+# - https://github.com/vmspereira/si
+# - https://github.com/cruz-f/si
 # -----------------------------------------------------------------------------------
