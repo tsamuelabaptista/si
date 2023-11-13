6ed5909130a5e505fae1060493092c2ee489ed75
João Correia <jfscorreia95@gmail.com>
2023-11-13 13:11:24 +0000

    [ADD] deal with different y formats

--- src/si/metrics/accuracy.py
+++ src/si/metrics/accuracy.py
@@ -17,4 +17,16 @@ def accuracy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     accuracy: float
         The accuracy of the model
     """
-    return np.sum(y_true == y_pred) / len(y_true)
+    # deal with predictions like [[0.52], [0.91], ...] and [[0.3, 0.7], [0.6, 0.4], ...]
+    # they need to be in the same format: [0, 1, ...] and [1, 0, ...]
+    def correct_format(y):
+        if len(y[0]) == 1:
+            corrected_y = [np.round(y[i][0]) for i in range(len(y))]
+        else:
+            corrected_y = [np.argmax(y[i]) for i in range(len(y))]
+        return np.array(corrected_y)
+    if isinstance(y_true[0], list) or isinstance(y_true[0], np.ndarray):
+        y_true = correct_format(y_true)
+    if isinstance(y_pred[0], list) or isinstance(y_pred[0], np.ndarray):
+        y_pred = correct_format(y_pred)
+    return np.sum(y_pred == y_true) / len(y_true)
