7ddc85490b38efcd6e5449eaf9ee2496dfacb265
Samuel Baptista <tsamuelab2001@gmail.com>
2024-10-04 00:50:42 +0100

    Adding my co-authorship to the package

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "João Capela"
-__credits__ = ["João Capela", "João Correia, Fernando Cruz", "Vítor Pereira"]
+__author__ = "Samuel Baptista"
+__credits__ = ["Samuel Baptista", "João Capela", "João Correia, Fernando Cruz", "Vítor Pereira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "João Capela"
-__email__ = "joao.capela@ceb.uminho.pt"
+__maintainer__ = "Samuel Baptista"
+__email__ = "pg49130@alunos.uminho.pt"
 
 # -----------------------------------------------------------------------------------
 # This package is heavily inspired and adapted from
