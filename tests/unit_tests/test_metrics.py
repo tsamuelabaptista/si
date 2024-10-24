798823c15b5d67e400b65f8e9be43ee1e13774e5
João Capela <pg38274@alunos.uminho.pt>
2024-10-24 13:34:53 +0100

    [ADD] logistic regression + sigmoid function

--- tests/unit_tests/test_metrics.py
+++ tests/unit_tests/test_metrics.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 import numpy as np
 from si.metrics.accuracy import accuracy
 from si.metrics.mse import mse
+from si.statistics.sigmoid_function import sigmoid_function
 
 class TestMetrics(TestCase):
 
@@ -21,3 +22,11 @@ class TestMetrics(TestCase):
         self.assertTrue(round(mse(y_true, y_pred), 3)==0.004)
         
 
+    def test_sigmoid_function(self):
+
+        x = np.array([1.9, 10.4, 75])
+
+        x_sigmoid = sigmoid_function(x)
+
+        self.assertTrue(all(x_sigmoid >= 0))
+        self.assertTrue(all(x_sigmoid <= 1))
